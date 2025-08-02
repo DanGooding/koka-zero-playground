47569b4f840f6682125264e38e56571f5047a76c
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-02 09:40:23 +0100

    Initial spring web project

---
+++ src/test/java/uk/danielgooding/koka_playground/KokaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.danielgooding.koka_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
