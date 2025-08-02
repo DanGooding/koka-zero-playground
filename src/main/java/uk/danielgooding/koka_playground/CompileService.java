200d5b6ce65c7fd3f0b05880170cd81d532b4da8
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-02 11:52:12 +0100

    Take compiler details as properties

--- src/main/java/uk/danielgooding/koka_playground/CompileService.java
+++ src/main/java/uk/danielgooding/koka_playground/CompileService.java
@@ -1,12 +1,20 @@
 package uk.danielgooding.koka_playground;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompileService {
+    @Value("${compiler.exe-path}")
+    private String exe_path;
+
+    @Value("${compiler.koka-zero-config-path}")
+    private String koka_zero_config_path;
+
+
     TypeCheckResult typecheck(KokaSourceCode sourceCode) {
         if (!sourceCode.getCode().contains("effect")) {
-            return TypeCheckResult.invalid("missing 'effect' keyword");
+            return TypeCheckResult.invalid(String.format("missing 'effect' keyword (%s)", exe_path));
         }
         return TypeCheckResult.valid();
     }
