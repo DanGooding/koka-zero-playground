98965f7dd7c743bdb2e8dbcd8d909321e8d7b997
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-02 12:19:42 +0100

    Shell out to compiler for typechecking

--- src/main/java/uk/danielgooding/koka_playground/CompileController.java
+++ src/main/java/uk/danielgooding/koka_playground/CompileController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class CompileController {
     @Autowired
     private CompileService compileService;
 
     @PostMapping(value = "/typecheck")
-    public TypeCheckResult typecheck(@RequestBody KokaSourceCode sourceCode) {
+    public TypeCheckResult typecheck(@RequestBody KokaSourceCode sourceCode) throws IOException, InterruptedException {
         return compileService.typecheck(sourceCode);
     }
 }
--- src/main/java/uk/danielgooding/koka_playground/CompileService.java
+++ src/main/java/uk/danielgooding/koka_playground/CompileService.java
@@ -3,6 +3,10 @@ package uk.danielgooding.koka_playground;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class CompileService {
     @Value("${compiler.exe-path}")
@@ -12,10 +16,23 @@ public class CompileService {
     private String koka_zero_config_path;
 
 
-    TypeCheckResult typecheck(KokaSourceCode sourceCode) {
-        if (!sourceCode.getCode().contains("effect")) {
-            return TypeCheckResult.invalid(String.format("missing 'effect' keyword (%s)", exe_path));
+    TypeCheckResult typecheck(KokaSourceCode sourceCode) throws IOException, InterruptedException {
+
+        ProcessBuilder processBuilder = new ProcessBuilder(exe_path, "check", "/dev/stdin");
+        Process process = processBuilder.start();
+
+        OutputStream stdin = process.getOutputStream();
+        stdin.write(sourceCode.getCode().getBytes(StandardCharsets.UTF_8));
+        stdin.close();
+
+        int exitCode = process.waitFor();
+
+        if (exitCode == 0) {
+            return TypeCheckResult.valid();
         }
-        return TypeCheckResult.valid();
+
+        String error = new String(process.getErrorStream().readAllBytes());
+
+        return TypeCheckResult.invalid(error);
     }
 }
