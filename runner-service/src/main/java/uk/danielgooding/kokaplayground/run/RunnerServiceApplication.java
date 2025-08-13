4e3537c6be17e8be37d6d75c715797e4e4feefd1
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-13 09:52:04 +0100

    Fix package scanning to point at dependencies

--- runner-service/src/main/java/uk/danielgooding/kokaplayground/run/RunnerServiceApplication.java
+++ runner-service/src/main/java/uk/danielgooding/kokaplayground/run/RunnerServiceApplication.java
@@ -3,7 +3,10 @@ package uk.danielgooding.kokaplayground.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "uk.danielgooding.kokaplayground.common",
+        "uk.danielgooding.kokaplayground.run",
+})
 public class RunnerServiceApplication {
 
     public static void main(String[] args) {
