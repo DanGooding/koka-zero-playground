d3576658e42a9dd26ceb531e65a146b37faee2f7
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-13 09:52:04 +0100

    Fix package scanning to point at dependencies

--- compile-service/src/main/java/uk/danielgooding/kokaplayground/compile/CompileServiceApplication.java
+++ compile-service/src/main/java/uk/danielgooding/kokaplayground/compile/CompileServiceApplication.java
@@ -3,7 +3,10 @@ package uk.danielgooding.kokaplayground.compile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "uk.danielgooding.kokaplayground.common",
+        "uk.danielgooding.kokaplayground.compile"
+})
 public class CompileServiceApplication {
 
     public static void main(String[] args) {
