f07bbc628ee7b39739c958c3d96a6194152c052d
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-14 16:25:37 +0100

    Add request logging

---
+++ common/src/main/java/uk/danielgooding/kokaplayground/common/CommonConfig.java
@@ -0,0 +1,19 @@
+package uk.danielgooding.kokaplayground.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class CommonConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
+}
