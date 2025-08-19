5bd998f7b8eda497678d78ec534829a3400f14ec
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-19 14:44:39 +0100

    Track basic state and update page

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,82 @@
 import './style.css'
-import {setupCounter} from './counter.ts'
-import sourceCode from './fibonacci-generator.kk?raw'
+import fibonacciGeneratorCode from './fibonacci-generator.kk?raw'
 
-document.querySelector<HTMLTextAreaElement>('#source-code')!.textContent = sourceCode
+const sourceCode = document.querySelector<HTMLTextAreaElement>('#source-code')!
+sourceCode.textContent = fibonacciGeneratorCode
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const runButton = document.querySelector<HTMLButtonElement>('#run-code')!
+
+// TODO: these are essentially reactive
+const runStatusDiv = document.querySelector<HTMLDivElement>('#run-status')!
+const errorDiv = document.querySelector<HTMLDivElement>('#error')!
+const outputDiv = document.querySelector<HTMLDivElement>('#output')!
+
+enum RunStatus {
+    Idle,
+    RequestedRun,
+    Compiling,
+    Running,
+}
+
+type State = {
+    runStatus: RunStatus,
+    output: string | null,
+    error: string | null,
+}
+
+
+function updateViewForRunStatus(status: RunStatus) {
+    var content: string;
+    switch (status) {
+        case RunStatus.Idle:
+            content = ""
+            break;
+        case RunStatus.RequestedRun:
+            content = "awaiting run..."
+            break
+        case RunStatus.Compiling:
+            content = "compiling..."
+            break
+        case RunStatus.Running:
+            content = "running..."
+            break
+    }
+    runStatusDiv.textContent = content
+}
+
+function updateViewForState(state: State) {
+    updateViewForRunStatus(state.runStatus)
+    outputDiv.textContent = state.output || ""
+    errorDiv.textContent = state.error || ""
+}
+
+var state: State = {
+    runStatus: RunStatus.Idle,
+    output: "",
+    error: ""
+}
+
+function runCode() {
+    state.runStatus = RunStatus.RequestedRun
+    state.output = null
+    state.error = null
+    updateViewForState(state)
+
+    setTimeout(() => {
+        state.runStatus = RunStatus.Compiling
+        updateViewForState(state)
+        setTimeout(() => {
+            state.runStatus = RunStatus.Running
+            updateViewForState(state)
+            setTimeout(() => {
+                state.error = "segfault :("
+                state.output = "hello\nworld\n:)"
+                state.runStatus = RunStatus.Idle
+                updateViewForState(state)
+            }, 300)
+        }, 300)
+    }, 300)
+}
+
+runButton.addEventListener('click', runCode)
+
