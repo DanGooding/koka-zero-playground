8e726fea433ca9e49471fb2990b76f97d71157c4
Dan Gooding <dan.gooding2000@gmail.com>
2025-08-28 21:25:55 +0000

    Provision droplet running nixos image

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,23 @@
+data "digitalocean_image" "nixos_vm_image" {
+  name = var.nixos_vm_image_name
+}
+
+data "digitalocean_ssh_key" "from_nixos_build_machine" {
+  name = "NixOS Build Machine"
+}
+
+resource "digitalocean_droplet" "main" {
+  image    = data.digitalocean_image.nixos_vm_image.id
+  name     = "main"
+  region   = var.region
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.from_nixos_build_machine.id]
+}
+
 resource "digitalocean_project" "koka_zero_playground" {
   name        = "Koka Zero Playground"
   description = "Online environment for running Koka Zero code"
   purpose     = "Web Application"
   environment = "Production"
-  resources   = []
+  resources   = [digitalocean_droplet.main.urn]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "region" {
   type    = string
   default = "lon1"
 }
+
+variable "nixos_vm_image_name" {
+  type    = string
+  default = "nixos-image-digital-ocean-25.11pre852965.c3e5d9f86b3f-x86_64-linux"
+}
