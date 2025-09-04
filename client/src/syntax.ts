989827125ebf1520758bec3e6483fa697cbd98c8
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-04 14:03:14 +0100

    Fix tokens to accept matches at start/end of file

--- client/src/syntax.ts
+++ client/src/syntax.ts
@@ -9,24 +9,24 @@ export const kokaGrammar: Grammar = {
     },
     number: /-?\b\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
     boolean: {
-        pattern: /(:?[^a-z0-9_-])(?:False|True)(?![a-z0-9_-])/,
+        pattern: /(:?[^a-z0-9_-])(?:False|True)(?=[^a-z0-9_-]|$)/,
         lookbehind: true,
     },
     keyword: {
-        pattern:/(:?[^a-z0-9_-])(fun|fn|with|effect|control|ctl|handler|handle|if|then|else|elif|val)(?![a-z0-9_-])/,
+        pattern:/(:?[^a-z0-9_-]|^)(fun|fn|with|effect|control|ctl|handler|handle|if|then|else|elif|val)(?=[^a-z0-9_-]|$)/,
         lookbehind: true,
     },
     builtin: {
-        pattern:/(:?[^a-z0-9_-])(resume)(?![a-z0-9_-])/,
+        pattern:/(:?[^a-z0-9_-]|^)(resume)(?=[^a-z0-9_-]|$)/,
         lookbehind: true,
     },
     // types
     "class-name": {
-        pattern: /(:?:\s*)(int|bool)(?![a-zA-Z0-9_-])/,
+        pattern: /(:?:\s*|^)(int|bool)(?=[^a-zA-Z0-9_-]|$)/,
         lookbehind: true,
     },
     identifier: {
-        pattern: /(:?[^a-z0-9_-])[a-z_][a-zA-Z0-9_-]*[a-zA-Z0-9_]?(?![a-z0-9_-])/,
+        pattern: /(:?[^a-z0-9_-]|^)[a-z_][a-zA-Z0-9_-]*[a-zA-Z0-9_]?(?=[^a-z0-9_-]|$)/,
         greedy: true,
         lookbehind: true,
     },
