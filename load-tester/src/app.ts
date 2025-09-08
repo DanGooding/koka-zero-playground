2df2bc53bc3e277a6adbadf62ca9241ef61aa1ec
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-08 17:20:58 +0100

    Compute effective concurrency

--- load-tester/src/app.ts
+++ load-tester/src/app.ts
@@ -6,13 +6,14 @@ import type {RequestDetails} from './outcomes.js'
 type cli_params = {
     url: URL,
     requestsPerSecond: number,
+    printRawStats: boolean,
 }
 
 function main(options: cli_params) {
     const requestor =
         (onOutcome: (summary: RequestDetails) => void) => sendRequest(options.url, onOutcome)
 
-    const loadTester = new LoadTester(options.requestsPerSecond, requestor)
+    const loadTester = new LoadTester(options.requestsPerSecond, requestor, options.printRawStats)
     loadTester.run()
 }
 
@@ -20,12 +21,14 @@ function cli() {
     const optionDefinitions = [
         { name: 'url', type: (url: string) => new URL(url) },
         { name: 'rps', type: Number, defaultValue: 1 },
+        { name: 'print-raw-stats', type: Boolean, defaultValue: false },
     ]
 
     const options = commandLineArgs(optionDefinitions)
     const params: cli_params = {
         url: options.url,
         requestsPerSecond: options.rps,
+        printRawStats: options['print-raw-stats']
     }
     main(params)
 }
