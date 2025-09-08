5cfbcc322a12d32046262d3be52792f66564f322
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-08 17:20:58 +0100

    Compute effective concurrency

--- load-tester/src/loadtester.ts
+++ load-tester/src/loadtester.ts
@@ -5,11 +5,17 @@ type Requestor = (onComplete: (summary: RequestDetails) => void) => void
 class BucketSummary {
     totalCount = 0
     outcomeCounts = new Map<RequestOutcome, number>()
+    totalOkLatencyMs: number = 0
 
     addSummary(summary: RequestDetails) {
         this.outcomeCounts.set(summary.outcome,
             (this.outcomeCounts.get(summary.outcome) ?? 0) + 1);
         this.totalCount += 1
+
+        if (summary.outcome === 'ok') {
+            // won't be undefined if request completed
+            this.totalOkLatencyMs += summary.toCloseMs ?? 0
+        }
     }
 
     getFractions(): Map<RequestOutcome, string> {
@@ -17,13 +23,14 @@ class BucketSummary {
         for (const [outcome, count] of this.outcomeCounts.entries()) {
             outcomeFractions.set(outcome, ((count / this.totalCount) * 100).toFixed(1))
         }
-        return  outcomeFractions
+        return outcomeFractions
     }
 
     static combine(buckets: BucketSummary[]) {
         const totalBucket = new BucketSummary()
         for (const bucket of buckets) {
             totalBucket.totalCount += bucket.totalCount
+            totalBucket.totalOkLatencyMs += bucket.totalOkLatencyMs
             for (const [outcome, count] of bucket.outcomeCounts) {
                 totalBucket.outcomeCounts.set(outcome,
                     (totalBucket.outcomeCounts.get(outcome) ?? 0) + count)
@@ -37,20 +44,20 @@ export default class LoadTester {
     readonly requestsPerSecond: number
     readonly sendRequest: Requestor
 
-    readonly windowSeconds: number
-    readonly bucketsPerWindow: number
+    readonly windowSeconds: number = 10
+    readonly bucketsPerWindow: number = 10
+
+    readonly printRawStats: boolean
 
     // the last bucket is currently being populated
     // its time window has not yet fully passed
     // so it shouldn't be used in calculations
     buckets: BucketSummary[]
 
-    constructor(requestsPerSecond: number, sendRequest: Requestor, windowSeconds: number = 10) {
+    constructor(requestsPerSecond: number, sendRequest: Requestor, printRawStats: boolean = false) {
         this.requestsPerSecond = requestsPerSecond
         this.sendRequest = sendRequest
-
-        this.bucketsPerWindow = 10
-        this.windowSeconds = windowSeconds
+        this.printRawStats = printRawStats
 
         this.buckets = [new BucketSummary()]
     }
@@ -62,12 +69,19 @@ export default class LoadTester {
         const windowBucket = BucketSummary.combine(allWindowBuckets)
         const windowSeconds = allWindowBuckets.length / this.bucketsPerWindow * this.windowSeconds
 
+        if (this.printRawStats) {
+            console.log(windowBucket)
+        }
+
         const outcomeFractions = windowBucket.getFractions()
         const effectiveRPS = (windowBucket.outcomeCounts.get('ok') ?? 0) / windowSeconds
+
+        const concurrency = windowBucket.totalOkLatencyMs / (windowSeconds * 1000)
 
         console.log({
             outcomeFractions,
             effectiveRPS,
+            concurrency
         })
     }
 
