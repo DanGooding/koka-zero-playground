2d698813a50c84bfce76de994bcba4eb19318a14
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-08 16:27:16 +0100

    Organise into modules

--- load-tester/src/app.ts
+++ load-tester/src/app.ts
@@ -1,82 +1,19 @@
 import commandLineArgs from 'command-line-args'
-
-type RequestOutcome = 'ok' | 'client error' | 'server error'
-type RequestSummary = {
-    outcome: RequestOutcome
-    toOpenMs?: number,
-    toFirstResponseMs?: number,
-    toCloseMs?: number,
-}
-
-function send_request(
-    url: URL,
-    onComplete: (requestSummary: RequestSummary) => void,
-) {
-    const ws = new WebSocket(url);
-
-    const openTime = Date.now()
-    let requestSummary: Partial<RequestSummary> = {}
-
-    ws.onopen = () => {
-        requestSummary.toOpenMs = Date.now() - openTime;
-        ws.send(JSON.stringify({
-            '@type': 'compile-and-run',
-            'code': 'fun main() { println-int(3 * 4); }'
-        }));
-    }
-
-    const onClose = (outcome: RequestOutcome) => {
-        if (requestSummary.outcome) return; // already closed
-        requestSummary.toCloseMs = Date.now() - openTime;
-        onComplete({ ...requestSummary, outcome: outcome });
-    }
-
-    ws.onmessage = (e) => {
-        requestSummary.toFirstResponseMs = Date.now() - openTime;
-        const message = JSON.parse(e.data);
-        if (message['@type'] === 'error') {
-            onClose('client error')
-        }
-    }
-
-    ws.onclose = (e) => {
-        // ok is NORMAL or GOING_AWAY
-        const closeOk = e.code == 1000 || e.code == 1001;
-        onClose(closeOk ? 'ok' : 'server error');
-    }
-}
+import {sendRequest} from './requestor.js'
+import LoadTester from './loadtester.js'
+import type {RequestDetails} from './outcomes.js'
 
 type cli_params = {
     url: URL,
-    requests_per_second: number,
+    requestsPerSecond: number,
 }
 
 function main(options: cli_params) {
+    const requestor =
+        (onOutcome: (summary: RequestDetails) => void) => sendRequest(options.url, onOutcome)
 
-    let totalCount = 0
-    const outcomeCounts = new Map<RequestOutcome, number>()
-
-    const onComplete = (summary: RequestSummary) => {
-        outcomeCounts.set(summary.outcome,
-            (outcomeCounts.get(summary.outcome) ?? 0) + 1);
-        totalCount += 1
-    }
-
-    setInterval(() => {
-        send_request(options.url, onComplete)
-    }, 1000 / options.requests_per_second)
-
-    const updatesPerSecond = 5
-    setInterval(() => {
-        const outcomeFractions: [RequestOutcome, string][] =
-            Array.from(outcomeCounts.entries())
-                .map(
-                    ([outcome, count]: [RequestOutcome, number]) => {
-                        return [outcome, (count / totalCount * 100).toFixed(1)];
-                    })
-        console.log(new Map<RequestOutcome, string>(outcomeFractions))
-
-    }, 1000 / updatesPerSecond)
+    const loadTester = new LoadTester(options.requestsPerSecond, requestor)
+    loadTester.run()
 }
 
 function cli() {
@@ -88,7 +25,7 @@ function cli() {
     const options = commandLineArgs(optionDefinitions)
     const params: cli_params = {
         url: options.url,
-        requests_per_second: options.rps,
+        requestsPerSecond: options.rps,
     }
     main(params)
 }
