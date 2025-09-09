6165fc613dd16de03ab7a289ac7399c3ac79bb21
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-09 10:38:58 +0100

    Implement load tester for compile service

--- load-tester/src/requestor.ts
+++ load-tester/src/requestor.ts
@@ -1,4 +1,9 @@
 import type { RequestDetails, RequestOutcome, RequestEvent } from './outcomes.js'
+import http from 'http'
+
+function secondsSince(then: number): number {
+    return (Date.now() - then) / 1000
+}
 
 export abstract class Requestor {
     url: URL
@@ -22,7 +27,7 @@ export class CompileAndRunRequestor extends Requestor {
         const toEventSeconds = new Map<(RequestEvent | 'first-response' | 'requested-run' | 'running'), number>()
 
         ws.onopen = () => {
-            toEventSeconds.set('opened', (Date.now() - openTime) / 1000)
+            toEventSeconds.set('opened', secondsSince(openTime))
             ws.send(JSON.stringify({
                 '@type': 'compile-and-run',
                 'code': 'fun main() { println-int(3 * 4); }'
@@ -32,12 +37,12 @@ export class CompileAndRunRequestor extends Requestor {
         const onClose = (outcome: RequestOutcome) => {
             if (toEventSeconds.has('closed')) return;
 
-            toEventSeconds.set('closed', (Date.now() - openTime) / 1000)
+            toEventSeconds.set('closed', secondsSince(openTime))
             onComplete({ toEventSeconds, outcome });
         }
 
         ws.onmessage = (e) => {
-            const secondsToNow = (Date.now() - openTime) / 1000
+            const secondsToNow = secondsSince(openTime)
             if (!toEventSeconds.has('first-response')) {
                 toEventSeconds.set('first-response', secondsToNow)
             }
@@ -66,6 +71,62 @@ export class CompileRequestor extends Requestor {
     }
 
     request(onComplete: (summary: RequestDetails) => void) {
-        console.log(`request ${this.url} ${onComplete}`)
+        const postData = JSON.stringify({code: 'fun main() { println-int(5 * 6); }'})
+
+        if (this.url.search) {
+            throw new Error('url query params not implemented: ' + this.url.search);
+        }
+        const options = {
+            host: this.url.hostname,
+            port: this.url.port,
+            path: this.url.pathname,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(postData),
+            },
+        };
+
+        const openTime = Date.now()
+        const toEventSeconds = new Map<RequestEvent | 'connected', number>()
+        let outcome: RequestOutcome
+
+        const req = http.request(options, (res) => {
+            const statusCode = res.statusCode ?? 0;
+            if (statusCode >= 200 && statusCode < 300) {
+                outcome = 'ok';
+            }else if (statusCode >= 500 && statusCode < 600) {
+                outcome = 'server error';
+            }else {
+                outcome = 'client error';
+            }
+
+            res.on('data', (chunk) => {
+                if (outcome) return;
+
+                // responses are small - assume they fit in one chunk
+                const result = JSON.parse(chunk.toString())
+                if (!result['ok']) {
+                    outcome = 'client error';
+                }
+            })
+        })
+
+        req.on('socket', () => {
+            toEventSeconds.set('connected', secondsSince(openTime))
+        })
+        req.on('error', () => {
+            outcome = 'transport error';
+        })
+        req.on('close', () => {
+            toEventSeconds.set('closed', secondsSince(openTime))
+            onComplete({
+                outcome: outcome ?? 'server error',
+                toEventSeconds
+            })
+        })
+
+        req.write(postData);
+        req.end();
     }
 }
