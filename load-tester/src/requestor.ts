b5e2de3edeca06ede1fe392dd6a67e226ee918b0
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-09 09:38:45 +0100

    Refactor Requestor to a class

--- load-tester/src/requestor.ts
+++ load-tester/src/requestor.ts
@@ -1,54 +1,71 @@
 import type { RequestDetails, RequestOutcome, RequestEvent } from './outcomes.js'
 
-export function sendCompileAndRunRequest(
-    url: URL,
-    onComplete: (requestDetails: RequestDetails) => void,
-) {
-    const ws = new WebSocket(url);
+export abstract class Requestor {
+    url: URL
 
-    const openTime = Date.now()
-    const toEventSeconds = new Map<(RequestEvent | 'first-response' | 'requested-run' | 'running'), number>()
-
-    ws.onopen = () => {
-        toEventSeconds.set('opened', (Date.now() - openTime) / 1000)
-        ws.send(JSON.stringify({
-            '@type': 'compile-and-run',
-            'code': 'fun main() { println-int(3 * 4); }'
-        }));
+    constructor(url: URL) {
+        this.url = url
     }
 
-    const onClose = (outcome: RequestOutcome) => {
-        if (toEventSeconds.has('closed')) return;
-
-        toEventSeconds.set('closed', (Date.now() - openTime) / 1000)
-        onComplete({ toEventSeconds, outcome });
-    }
-
-    ws.onmessage = (e) => {
-        const secondsToNow = (Date.now() - openTime) / 1000
-        if (!toEventSeconds.has('first-response')) {
-            toEventSeconds.set('first-response', secondsToNow)
-        }
-        const message = JSON.parse(e.data);
-        if (message['@type'] === 'error') {
-            onClose('client error')
-        }else if (message['@type'] === 'starting-run') {
-            toEventSeconds.set('requested-run', secondsToNow)
-        }else if (message['@type'] === 'running') {
-            toEventSeconds.set('running', secondsToNow)
-        }
-    }
-
-    ws.onclose = (e) => {
-        // ok is NORMAL or GOING_AWAY
-        const closeOk = e.code == 1000 || e.code == 1001;
-        onClose(closeOk ? 'ok' : 'server error');
-    }
+    abstract request(onComplete: (summary: RequestDetails) => void): void
 }
 
-export function sendCompileRequest(
-    url: URL,
-    onComplete: (requestDetails: RequestDetails) => void
-) {
-    console.log(`request ${url} ${onComplete}`)
+export class CompileAndRunRequestor extends Requestor {
+    constructor(url: URL) {
+        super(url)
+    }
+
+    request(onComplete: (summary: RequestDetails) => void) {
+        const ws = new WebSocket(this.url);
+
+        const openTime = Date.now()
+        const toEventSeconds = new Map<(RequestEvent | 'first-response' | 'requested-run' | 'running'), number>()
+
+        ws.onopen = () => {
+            toEventSeconds.set('opened', (Date.now() - openTime) / 1000)
+            ws.send(JSON.stringify({
+                '@type': 'compile-and-run',
+                'code': 'fun main() { println-int(3 * 4); }'
+            }));
+        }
+
+        const onClose = (outcome: RequestOutcome) => {
+            if (toEventSeconds.has('closed')) return;
+
+            toEventSeconds.set('closed', (Date.now() - openTime) / 1000)
+            onComplete({ toEventSeconds, outcome });
+        }
+
+        ws.onmessage = (e) => {
+            const secondsToNow = (Date.now() - openTime) / 1000
+            if (!toEventSeconds.has('first-response')) {
+                toEventSeconds.set('first-response', secondsToNow)
+            }
+            const message = JSON.parse(e.data);
+            if (message['@type'] === 'error') {
+                onClose('client error')
+            }else if (message['@type'] === 'starting-run') {
+                toEventSeconds.set('requested-run', secondsToNow)
+            }else if (message['@type'] === 'running') {
+                toEventSeconds.set('running', secondsToNow)
+            }
+        }
+
+        ws.onclose = (e) => {
+            // ok is NORMAL or GOING_AWAY
+            const closeOk = e.code == 1000 || e.code == 1001;
+            onClose(closeOk ? 'ok' : 'server error');
+        }
+    }
+
+}
+
+export class CompileRequestor extends Requestor {
+    constructor(url: URL) {
+        super(url);
+    }
+
+    request(onComplete: (summary: RequestDetails) => void) {
+        console.log(`request ${this.url} ${onComplete}`)
+    }
 }
