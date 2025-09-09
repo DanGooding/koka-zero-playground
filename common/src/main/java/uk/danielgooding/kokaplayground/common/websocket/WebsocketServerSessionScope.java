bcd25e960281564eb142490e57ae96f9249bd498
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-09 17:09:24 +0100

    Remove unused imports

--- common/src/main/java/uk/danielgooding/kokaplayground/common/websocket/WebsocketServerSessionScope.java
+++ common/src/main/java/uk/danielgooding/kokaplayground/common/websocket/WebsocketServerSessionScope.java
@@ -1,31 +1,27 @@
 package uk.danielgooding.kokaplayground.common.websocket;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class WebsocketServerSessionScope implements Scope {
     public static final String REFERENCE = "websocket-server-session";
-    private static final Logger logger = LoggerFactory.getLogger(WebsocketServerSessionScope.class);
-
 
     private final Map<String, Map<String, Object>> beansByNameByScope;
 
     public WebsocketServerSessionScope() {
-        this.beansByNameByScope = new HashMap<>();
+        this.beansByNameByScope = new ConcurrentHashMap<>();
     }
 
     @Override
     public @NonNull Object get(@NonNull String beanName, @NonNull ObjectFactory<?> beanFactory) {
         Map<String, Object> beansByName =
-                beansByNameByScope.computeIfAbsent(getConversationId(), k -> new HashMap<>());
+                beansByNameByScope.computeIfAbsent(getConversationId(), k -> new ConcurrentHashMap<>());
 
         return beansByName.computeIfAbsent(beanName, ignored -> beanFactory.getObject());
     }
@@ -39,9 +35,11 @@ public class WebsocketServerSessionScope implements Scope {
 
     @Override
     public void registerDestructionCallback(@NonNull String name, @NonNull Runnable callback) {
-        WebsocketServerSessionHolder.getInstance().getSession().getOutcomeFuture().whenComplete((result, exn) -> {
-            callback.run();
-        });
+        WebsocketServerSessionHolder
+                .getInstance()
+                .getSession()
+                .getOutcomeFuture()
+                .whenComplete((result, exn) -> callback.run());
     }
 
     @Override
