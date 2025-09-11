0093e1c33b175b6322d4125e94c2749659e1d327
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-11 08:00:46 +0100

    Add custom metric for compiler-tool latency

--- app/compile-service-app/src/main/java/uk/danielgooding/kokaplayground/compile/CompileServiceConfig.java
+++ app/compile-service-app/src/main/java/uk/danielgooding/kokaplayground/compile/CompileServiceConfig.java
@@ -1,8 +1,17 @@
 package uk.danielgooding.kokaplayground.compile;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class CompileServiceConfig {
 
+    @Bean
+    TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
 }
