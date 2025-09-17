0e67057c30a242eeead37fb9878dd3795be7d79d
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-17 18:37:47 +0100

    Put redis cache in front of koka compiler

--- app/compile-service-app/src/main/java/uk/danielgooding/kokaplayground/compile/CompileServiceConfig.java
+++ app/compile-service-app/src/main/java/uk/danielgooding/kokaplayground/compile/CompileServiceConfig.java
@@ -1,10 +1,20 @@
 package uk.danielgooding.kokaplayground.compile;
 
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -13,5 +23,32 @@ public class CompileServiceConfig {
     @Bean
     TimedAspect timedAspect(MeterRegistry registry) {
         return new TimedAspect(registry);
+    }
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory(
+            @Value("${compile.exe-cache.hostname}") String exeCacheHostname,
+            @Value("${compile.exe-cache.port}") int exeCachePort) {
+
+        // a 'standalone' redis instance (not clustered)
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(exeCacheHostname, exeCachePort);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    RedisTemplate<ExeCacheKey, byte[]> exeCacheRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<ExeCacheKey, byte[]> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(connectionFactory);
+
+        // it's important that json fields are serialised in a stable order within keys
+        Jackson2ObjectMapperBuilder objectMapperBuilder = new Jackson2ObjectMapperBuilder();
+        objectMapperBuilder.featuresToEnable(MapperFeature.SORT_CREATOR_PROPERTIES_BY_DECLARATION_ORDER);
+        ObjectMapper objectMapper = objectMapperBuilder.build();
+
+        template.setKeySerializer(new Jackson2JsonRedisSerializer<>(objectMapper, ExeCacheKey.class));
+        template.setValueSerializer(RedisSerializer.byteArray());
+
+        return template;
     }
 }
--- service/compile-service/src/main/java/uk/danielgooding/kokaplayground/compile/CompileService.java
+++ service/compile-service/src/main/java/uk/danielgooding/kokaplayground/compile/CompileService.java
@@ -1,51 +1,79 @@
 package uk.danielgooding.kokaplayground.compile;
 
-import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import uk.danielgooding.kokaplayground.common.*;
 import uk.danielgooding.kokaplayground.common.exe.ExeHandle;
 import uk.danielgooding.kokaplayground.common.exe.ExeStore;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 @Service
 public class CompileService {
+    @Autowired
+    private ExeCache exeCache;
+
     @Autowired
     private CompilerTool compilerTool;
 
     @Autowired
     private ExeStore exeStore;
 
+    private static final Logger logger = LoggerFactory.getLogger(CompileService.class);
+
     public CompletableFuture<OrError<Void>> typecheck(KokaSourceCode sourceCode) {
         return compilerTool.typecheck(sourceCode);
     }
 
     public CompletableFuture<OrError<ExeHandle>> compile(KokaSourceCode sourceCode, boolean optimise) {
-        CompletableFuture<OrError<Path>> result = compilerTool.compile(sourceCode, optimise);
 
-        return result.thenCompose(
-                (maybeLocalExe) -> {
-                    switch (maybeLocalExe) {
-                        case Failed<?> error -> {
-                            return CompletableFuture.completedFuture(error.castValue());
-                        }
-                        case Ok<Path> localExe -> {
-                            try {
-                                ExeHandle handle = exeStore.putExe(localExe.getValue());
-                                return CompletableFuture.completedFuture(OrError.ok(handle));
-                            } catch (IOException e) {
-                                return CompletableFuture.failedFuture(e);
+        Optional<byte[]> compiledExe = exeCache.getCompiledExe(sourceCode, optimise);
+
+        if (compiledExe.isEmpty()) {
+            logger.info("exe not found in cache, compiling");
+
+            CompletableFuture<OrError<Path>> result = compilerTool.compile(sourceCode, optimise);
+
+            return result.thenCompose(
+                    (maybeLocalExe) -> {
+                        switch (maybeLocalExe) {
+                            case Failed<?> error -> {
+                                // we don't cache errors
+                                return CompletableFuture.completedFuture(error.castValue());
                             }
+                            case Ok<Path> localExe -> {
+                                try {
+                                    byte[] exe = Files.readAllBytes(localExe.getValue());
+                                    exeCache.putCompiledExe(sourceCode, optimise, exe);
+
+                                    return uploadAndReturnExe(exe);
+                                } catch (IOException e) {
+                                    return CompletableFuture.failedFuture(e);
+                                }
+                            }
+
                         }
-
                     }
-                }
-        );
+            );
 
+        } else {
+            logger.info("exe found in cache, skipping compiler");
+            return uploadAndReturnExe(compiledExe.get());
+        }
+    }
+
+    public CompletableFuture<OrError<ExeHandle>> uploadAndReturnExe(byte[] exe) {
+        try {
+            ExeHandle handle = exeStore.putExe(exe);
+            return CompletableFuture.completedFuture(OrError.ok(handle));
+        } catch (IOException e) {
+            return CompletableFuture.failedFuture(e);
+        }
     }
 }
