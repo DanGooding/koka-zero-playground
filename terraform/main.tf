8585ba9e766f54a3213cf99b798303391ad400ca
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-19 13:09:33 +0100

    Create digitalocean uptime checks

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,3 +37,22 @@ resource "digitalocean_project" "koka_zero_playground" {
   environment = "Production"
   resources = [digitalocean_droplet.main.urn]
 }
+
+resource "digitalocean_uptime_check" "koka_zero_playground" {
+  name   = "koka-zero-playground uptime check"
+  target = "https://koka-zero.danielgooding.uk"
+  regions = ["eu_west"]
+}
+
+resource "digitalocean_uptime_alert" "koka_zero_playground" {
+  check_id   = digitalocean_uptime_check.koka_zero_playground.id
+  name = "koka-zero-playground uptime alert"
+  # alert once down for 10 min
+  type       = "down"
+  period     = "10m"
+  comparison = "less_than"
+  threshold  = 1
+  notifications {
+    email = [var.alert_email]
+  }
+}
